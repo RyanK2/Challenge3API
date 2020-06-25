896ba627726a307c83ccda002e0411167b78b719
RyanK2 <16060695@student.hhs.nl>
2020-06-25 17:55:19 +0200

    finished close

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 var myMap;
+var input = document.querySelector('.input_text');
+var main = document.querySelector('#city');
+var temp = document.querySelector('.temp');
+var desc = document.querySelector('.desc');
+var clouds = document.querySelector('.clouds');
+var button= document.querySelector('.submit');
 
 function initMap() {
 
@@ -22,3 +28,21 @@ function initMap() {
 	});
 
 }
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=50a7aa80fa492fa92e874d23ad061374&units=metric')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  desc.innerHTML = descValue;
+  temp.innerHTML = tempValue + ' ºC';
+  input.value = "";
+
+})
+
+.catch(err => alert("This city doesn't exist. (Control the spelling)"));
+})
